cc885594e9c66f2a4c427f4e974bdf6d33d0325d
C.Roberts <chanceton.roberts225@gmail.com>
2022-07-31 21:38:29 -0500

    First update

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
+let lowercasearray=["a","b","c","d","e","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+let capitalcasearray=["A","B","C","D","E","F","G",'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+let numbersarray=["0","1",'2','3','4','5','6','7','8','9']
+let specialcarrecterarray=["!","@",'#','$','%','^','&','*','(',')','-','_','=','+','?','<','>','[',']','{','}']
+let lowercaseboolean;
+let capitalcaseboolean;
+let numbersboolean;
+let specialcarrecterboolean;
+let validcaracters=[]
+  
+  // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +19,32 @@ function writePassword() {
 
 }
 
+function generatePassword(length){
+  if(lowercaseboolean){
+    validcaracters.push(lowercasearray)
+
+  }else if(capitalcaseboolean){
+    validcaracters.push (capitalcasearray)
+  }else if(numbersboolean){
+    validcaracters.push (numbersarray)
+  }else if ( specialcarrecterboolean){
+    validcaracters.push(specialcarrecterarray)
+  } 
+  
+}
+
+function initialquestions() {
+  let lengthofpassword= prompt("Enter password legnth");
+  
+  
+
+  lowercaseboolean=confirm("Do you want lowercase?");
+  capitalcaseboolean=confirm("Do you want capitals?")
+  numbersboolean=confirm("Do you want numbers?")
+  specialcarrecterboolean=confirm("Do you want special caracters?")
+  generatePassword(lengthofpassword)
+
+}
+initialquestions()
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
